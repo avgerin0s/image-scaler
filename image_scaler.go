20f6c2bef49c19efd48a9414d5df854533128dcc
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2015-02-24 18:30:23 +0200

    Gofmt & Golint

--- image_scaler.go
+++ image_scaler.go
@@ -1,116 +1,117 @@
 package main
 
 import (
-  "github.com/rakyll/magicmime"
-  "github.com/disintegration/imaging"
-  "github.com/gorilla/mux"
-  "runtime"
-  "fmt"
-  "os"
-  "io"
-  "log"
-  "strings"
-  "strconv"
-  "regexp"
-  "net/http"
-  "crypto/md5"
-  "encoding/hex"
-  "encoding/json"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/disintegration/imaging"
+	"github.com/gorilla/mux"
+	"github.com/rakyll/magicmime"
 )
 
+// Image represents the JSON response of the api.
 type Image struct {
-  Url string
+	URL string
 }
 
 func Resize(w http.ResponseWriter, req *http.Request) {
-  url := req.URL.Query().Get("url")
-  param_width := req.URL.Query().Get("width")
-  param_height := req.URL.Query().Get("height")
+	url := req.URL.Query().Get("url")
+	paramWidth := req.URL.Query().Get("width")
+	paramHeight := req.URL.Query().Get("height")
 
-  log.Println("Downloading url: " + url)
+	log.Println("Downloading url: " + url)
 
-  var width int
-  var height int
+	if url == "" {
+		panic("Url is empty")
+	}
 
-  if url == "" {
-    panic("Url is empty")
-  }
+	var width int
+	if paramWidth == "" {
+		width = 1280
+	} else {
+		width, _ = strconv.Atoi(paramWidth)
+	}
 
-  if param_width == "" {
-    width = 1280
-  } else {
-    width, _ = strconv.Atoi(param_width)
-  }
+	var height int
+	if paramHeight == "" {
+		height = 0
+	} else {
+		height, _ = strconv.Atoi(paramHeight)
+	}
 
-  if param_height == "" {
-    height = 0
-  } else {
-    height, _ = strconv.Atoi(param_height)
-  }
+	mm, err := magicmime.New(magicmime.MAGIC_MIME_TYPE | magicmime.MAGIC_ERROR)
+	if err != nil {
+		panic(err)
+	}
 
-  mm, err := magicmime.New(magicmime.MAGIC_MIME_TYPE | magicmime.MAGIC_ERROR)
-  if err != nil {
-    panic(err)
-  }
+	// Download the url
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 
-  // Download the url
-  resp, err := http.Get(url)
-  if err != nil {
-    panic(err)
-  }
-  defer resp.Body.Close()
+	// Create an md5 checksum for filename
+	hasher := md5.New()
+	hasher.Write([]byte(url))
+	originalFilename := hex.EncodeToString(hasher.Sum(nil))
+	originalFile, err := os.Create(originalFilename + ".jpeg")
+	io.Copy(originalFile, resp.Body)
 
-  // Create an md5 checksum for filename
-  hasher := md5.New()
-  hasher.Write([]byte(url))
-  originalFilename := hex.EncodeToString(hasher.Sum(nil))
-  originalFile, err := os.Create(originalFilename + ".jpeg")
-  io.Copy(originalFile, resp.Body)
+	// Determine content-type of downloaded file
+	mimetype, err := mm.TypeByFile(originalFilename + ".jpeg")
+	isImage, _ := regexp.MatchString("^image/", mimetype)
+	extension := strings.Split(mimetype, "/")[1]
+	defer originalFile.Close()
+	if !isImage {
+		fmt.Println("Not an image file")
+		return
+	}
 
-  // Determine content-type of downloaded file
-  mimetype, err := mm.TypeByFile(originalFilename + ".jpeg")
-  isImage, _ := regexp.MatchString("^image/", mimetype)
-  extension := strings.Split(mimetype, "/")[1]
-  defer originalFile.Close()
-  if !isImage {
-    fmt.Println("Not an image file")
-    return
-  }
+	// Process the image
+	img, err := imaging.Open(originalFilename + ".jpeg")
+	if err != nil {
+		panic(err)
+	}
 
-  // Process the image
-  img, err := imaging.Open(originalFilename + ".jpeg")
-  if err != nil {
-    panic(err)
-  }
+	resizedImage := imaging.Resize(img, width, height, imaging.Lanczos)
+	newFileName := "resized_" + originalFilename + "." + extension
 
-  resizedImage := imaging.Resize(img, width, height, imaging.Lanczos)
-  newFileName := "resized_" + originalFilename + "." + extension
+	imaging.Save(resizedImage, newFileName)
 
-  imaging.Save(resizedImage, newFileName)
+	image := Image{"http://127.0.0.1:3000/static/" + newFileName}
+	js, err := json.Marshal(image)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  image := Image{"http://127.0.0.1:3000/static/" + newFileName}
-  js, err := json.Marshal(image)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-
-  w.Header().Set("Content-Type", "application/json")
-  w.Write(js)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func main() {
-  // Use all available CPUs
-  runtime.GOMAXPROCS(runtime.NumCPU())
+	// Use all available CPUs
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-  r := mux.NewRouter()
+	r := mux.NewRouter()
 
-  r.HandleFunc("/resize", Resize)
-  http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("."))))
-  http.Handle("/", r)
+	r.HandleFunc("/resize", Resize)
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("."))))
+	http.Handle("/", r)
 
-  err := http.ListenAndServe(":3000", nil)
-  if err != nil {
-    log.Fatal("ListenAndServe:", err)
-  }
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
